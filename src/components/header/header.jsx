bcb537b109fa2303324ec6a8dd7da04d1d323233
Alberto Caminos <albeerc@gmail.com>
2022-09-20 13:01:51 -0300

    E-commerce con react

---
+++ src/components/header/header.jsx
@@ -0,0 +1,15 @@
+import './header.css'
+
+function Header () {
+    return (
+        <header className='header'>
+        <h1>Hola mundo</h1>
+        <ul>
+            <li>Tienda</li>
+            <li>Catálogo</li>
+        </ul>
+        </header>
+    );
+}
+
+export default Header
